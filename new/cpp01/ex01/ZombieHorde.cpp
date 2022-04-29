4299548ac27ffe15b629f58f3bb487c7c333fed7
Swenne @Codam <swofferh@student.codam.nl>
2022-04-29 21:23:48 +0200

    wont

--- new/cpp01/ex01/ZombieHorde.cpp
+++ new/cpp01/ex01/ZombieHorde.cpp
@@ -2,16 +2,7 @@
 #include "Zombie.hpp"
 
 Zombie* zombieHorde(int N, std::string name){
-    std::string names[] = 
-    {
-        "Mario"
-        "Carlo"
-        "Luigi"
-        "Julio"
-        "Pablo"
-    };
-    std::string types[] = 
-    {
+    std::string types[] = {
         "nerd eater"
         "shooter"
         "speedo"
@@ -20,16 +11,10 @@ Zombie* zombieHorde(int N, std::string name){
         "brainiac"
         "creeper"
     };
-    horde = new Zombie[N];
-    for (int i = 0; i < N; i++){
-        std::srand(std::time(NULL));
-        horde[i].setName(names[std::rand() % 5]);
-        horde[i].setType(types[std::rand() % 7]);
+    Zombie *horde;
+    for(int i = 0; i < N; i++){
+        horde[i].setName(name);
+        //horde[i].setType(types[std::rand() % 7]);
     }
-}
-
-void Zombie::announce(){
-    for (int i = 0; i < counter; i++){
-        horde[i].announce();
-    }
+    return (new Zombie[N]);
 }
