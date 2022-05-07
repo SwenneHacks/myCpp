3410165521eb9a02ed3a827b3eb5b01960e64eb4
Swenne @Codam <swofferh@student.codam.nl>
2022-05-07 19:33:48 +0200

    for loop

--- new/ex05/main.cpp
+++ new/ex05/main.cpp
@@ -4,19 +4,16 @@
 int main()
 {
 	Harl Karen;
-	int i = 0;
 	std::string check[] = {"DEBUG","INFO","WARNING","ERROR"};
 	std::string command;
 	std::cout << "Give Harl a command: ";
 	std::cin >> command;
-	while(i < 4){
+	for (int i = 0; i < 4; i++){
 		if (command == check[i]){
 			Karen.complain(command);
 			return 0;
 		}
-		i++;
 	}
-	if (command != check[i])
-		std::cout << BLUE << "Wrong command, Karen!" << std::endl;
+	std::cout << BLUE << "Wrong command, Karen!" << std::endl;
 	return 0;
 }
