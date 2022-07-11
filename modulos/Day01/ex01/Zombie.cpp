760f745dd20388ced90b2e53d749c6625bff0a81
Sue <54895147+SwenneHacks@users.noreply.github.com>
2022-07-11 19:17:28 +0200

    2020 exercises

---
+++ modulos/Day01/ex01/Zombie.cpp
@@ -0,0 +1,19 @@
+
+#include "Zombie.hpp"
+
+Zombie::Zombie(){
+	//std::cout << "[ A zombie raised out of a constructor's grave ]." << std::endl;
+}
+
+
+void Zombie::announce() {
+	std::cout << Zombie::_name << " > Braiiiiiiinnnssss..." << std::endl;
+}
+
+void Zombie::setName(std::string name){
+	this->_name = name;
+}
+
+Zombie::~Zombie(){
+	//std::cout << "[ " << Zombie::_name << " killed by a destructor." << std::endl;
+}
