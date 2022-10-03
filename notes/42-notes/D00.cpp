cae868ea4e9d1eab3866ed1a5156dfa664f1f887
Sue <54895147+SwenneHacks@users.noreply.github.com>
2022-10-03 18:46:22 +0200

    notes / precticing

--- notes/42-notes/D00.cpp
+++ notes/42-notes/D00.cpp
@@ -1,43 +1,22 @@
 #include <iostream>
 
-struct Struct {
-    int foo;
-};
+std::string Y = "\x1b[33m";
+std::string B = "\x1b[34m";
+std::string R = "\x1b[0m";
+
 
 //    indentifier, data-structure:
 class Class {
 public:
-    int   var;
     Class(void);
     ~Class(void);
     void function(void);
     void bar(void)const;
+    int  var;
 };
-
-class Const {
-public:
-    float const pi;
-    int         qd;
-    Const(float const f);
-    ~Const(void);
-    void f(void)const;
-};
-
-class Sample {
-public:
-    char    x;
-    int     y;
-    float   z;
-    Sample(char a, int b, float c);
-    ~Sample();
-    static int getInstNum(void);
-private:
-    static int _InstNum;
-};
-
 
 //      always finish with ;
-
+//      above .HPP bellow .CPP
 
 Class::Class(void) : var(40) {
     std::cout << "Class Constructor called" << std::endl;
@@ -60,6 +39,20 @@ void Class::bar(void) const{
     std::cout << "another var pointer here: " << this->var << std::endl;
 }
 
+//________________________________________________________________ SAMPLE
+
+class Sample {
+public:
+    char    x;
+    int     y;
+    float   z;
+    Sample(char a, int b, float c);
+    ~Sample();
+    static int getInstNum(void);
+private:
+    static int _InstNum;
+};
+
 Sample::Sample(char a, int b, float c) : x(a), y(b), z(c){
     std::cout << "Sample Constructor called" << std::endl;
     std::cout << "Number of instances: " << Sample::getInstNum() << std::endl;
@@ -79,7 +72,16 @@ int Sample::getInstNum(void){
     return Sample::_InstNum;
 }
 
-int Sample::_InstNum = 0;
+//_______________________________________________________________ COSNT 
+
+class Const {
+public:
+    float const pi;
+    int         qd;
+    Const(float const f);
+    ~Const(void);
+    void f(void)const;
+};
 
 Const::Const(float const f) : pi(f), qd(42) {
     std::cout << "Const Constructor called" << std::endl;
@@ -94,62 +96,64 @@ void Const::f(void)const {
     std::cout << "this->qd = " << this->qd << std::endl;
 }
 
+//_______________________________________________________________ STACK non member funtion/attributes 
 
-// above the class files, where you can define the namespaces
+struct Struct { int foo;};
 
-
+int Sample::_InstNum = 0;
 
 void f(void){
     Sample instance2('b', 44, 3.2f);
     std::cout << "Number of instances: " << Sample::getInstNum() << std::endl;
 }
 
-
-// above an example of non member funtion/attributes - useful to count how many instances have been made.
-
-
-
 int main()
 {
     Class instance;
     Class * pointer = &instance;
-    std::cout << "Now we're at main. var: " << instance.var << std::endl;
+    std::cout << B << "Now we're at main. var: " << R << instance.var << std::endl;
 
     int Class::*pp = NULL; // pointing to an integer member of the 'sample' class intialized to null;
     void (Class::*ff)(void)const; // pointer to function member, making an assignment. Specifying which instance bellow.
     ff = &Class::bar; // taking the address of the 'sample' class bar function mamber (not the other function)
     pp = &Class::var; // taking the address of the 'sample' class var member. Great way to assign new values.
     
-    instance.*pp = 48; // .* operator is used for updating attribute directly to the STACK instance specified (above)
-    std::cout << "Pointer to STACK var: " << instance.var << std::endl;
-    pointer->*pp = 42; // -> operator used to point to a class pointer instance, directing it to instance address.
-    std::cout << "Pointer to instance address: " << instance.var << std::endl;
-    (pointer->*ff)(); // calling bar function via pointer to member function on instance pointer.
-    std::cout << "Pointer to function bar (const): " << instance.var << std::endl;
+    instance.*pp = 48; 
+    // .* operator is used for updating attribute directly to the STACK instance specified (above)
+    std::cout <<B<< "Pointer to STACK var: " <<R<< instance.var << std::endl;
+    
+    pointer->*pp = 42; 
+    // -> operator used to point to a class pointer instance, directing it to instance address.
+    std::cout <<B<< "Pointer to instance address: " <<R<< instance.var << std::endl;
+    
+    (pointer->*ff)(); 
+    // calling bar function via pointer to member function on instance pointer.
+    std::cout <<B<< "Pointer to function bar (const): "<<R<< instance.var << std::endl;
 
     instance.var = 44;
-    std::cout << "Now declared STACK var: " << instance.var << std::endl;
+    std::cout <<B<< "Now declared STACK var: " <<R<< instance.var << std::endl;
     instance.function();
-    std::cout << "Var after fuction call: " << instance.var << std::endl;
+    std::cout <<B<< "Var after fuction call: " <<R<< instance.var << std::endl;
     
     //
     
     Sample instance1('a', 42, 4.2f);
     f();
-    std::cout << "here -> x = " << instance1.x << std::endl; 
-    std::cout << "here -> y = " << instance1.y << std::endl;
-    std::cout << "here -> z = " << instance1.z << std::endl;
+    std::cout <<B<< "here -> x = " <<R<< instance1.x << std::endl; 
+    std::cout <<B<< "here -> y = " <<R<< instance1.y << std::endl;
+    std::cout <<B<< "here -> z = " <<R<< instance1.z << std::endl;
     
     //
-
+    
     Const instance3(3.14f);
     instance3.f();
     
     //
+
     Struct S;
-    std::cout << "Not initialized Struct foo: " << S.foo << std::endl;
+    std::cout <<B<< "Not initialized Struct foo: " <<R<< S.foo << std::endl;
     S.foo = 42;
-    std::cout << "Leaving main. declared foo: " << S.foo << std::endl;
+    std::cout <<B<< "Leaving main. declared foo: " <<R<< S.foo << std::endl;
     return 0;
 }
 
