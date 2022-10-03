aa6100757d5b89ec75a9fc285e221f448a0a3573
Sue <54895147+SwenneHacks@users.noreply.github.com>
2022-10-03 18:46:53 +0200

    starting mod02

--- exercises/Mod02/ex00/main.cpp
+++ exercises/Mod02/ex00/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "Fixed.hpp"
 
-int main( void ) 
+void test(void) 
 {
 Fixed a;
 Fixed b( a );
@@ -11,8 +11,11 @@ std::cout << a.getRawBits() << std::endl;
 std::cout << b.getRawBits() << std::endl;
 std::cout << c.getRawBits() << std::endl;
 
-std::cout << RED <<"_________" << BLUE << "MY TEST" 
-<< RED << "________\n" << RESET << std::endl;
+std::cout << RED 
+<<"_________" << BLUE 
+<< "MY TEST" << RED 
+<< "________\n" << RESET 
+<< std::endl;
 
 a.setRawBits(1);
 a = b;
@@ -23,3 +26,22 @@ std::cout << b.getRawBits() <<" = b"<< std::endl;
 std::cout << c.getRawBits() <<" = c"<< std::endl;
 return 0;
 }
+
+int		main(int ac, char **av)
+{
+	test();
+	std::cout << std::endl;
+	std::string	s = av[0];
+	s = "leaks " + s.erase(0,2);
+	if (ac > 1 && !strcmp(av[1],"leaks"))
+		system(s.c_str());
+	else
+	{
+		s += " >> leaks.txt";
+		system(s.c_str());
+		system("grep \"total leaked bytes\" leaks.txt");
+		system("rm leaks.txt");
+	}
+	return (0);
+}
+
