ad8c7c2543d2276e3c6708b242d3f3dc89580b8b
Sue <54895147+SwenneHacks@users.noreply.github.com>
2022-10-10 19:27:14 +0200

    made a start to mod02

--- exercises/Mod02/ex01/Fixed.cpp
+++ exercises/Mod02/ex01/Fixed.cpp
@@ -7,12 +7,14 @@ const int Fixed::_fractional_bits = 8;
 
 Fixed::Fixed(void) : _fixed_point_value(0){
 	std::cout << "Default constructor called" << std::endl;
+
 	return ;
 }
 
 Fixed::Fixed(Fixed const &src){
 	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
+
 	return ;
 }
 
@@ -34,16 +36,17 @@ Fixed &	Fixed::operator=(Fixed const &rhs){
 	std::cout << "Assignation operator called" << std::endl;
 	if (this != &rhs)
 		this->_fixed_point_value = rhs.getRawBits();
-
 	return (*this);
 }
 
 int	Fixed::getRawBits(void) const{
+
 	return (this->_fixed_point_value);
 }
 
 void Fixed::setRawBits(int const raw){
 	this->_fixed_point_value = raw;
+
 	return ;
 }
 
@@ -51,18 +54,22 @@ std::ostream & 		operator<<(std::ostream & out, Fixed const & in);
 
 std::ostream&	operator<<(std::ostream & out, Fixed const & in){
 	out << in.toFloat();
+
 	return (out);
 }
 
-float Fixed::toFloat( void ) const{
+float Fixed::toFloat( void ) const{ 
+
 	return (static_cast<float>(_fixed_point_value) / (1 << _fractional_bits));
 }
 
 int Fixed::toInt( void ) const{
+
 	return (_fixed_point_value / (1 << _fractional_bits));
 }
 
 Fixed::~Fixed(void){
 	std::cout << "Destructor called" << std::endl;
+
 	return ;
 }
