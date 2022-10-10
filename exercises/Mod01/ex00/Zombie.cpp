64d34aadbf739953b0d9ca54f1e319a9fadf9e5b
Sue <54895147+SwenneHacks@users.noreply.github.com>
2022-10-10 19:27:53 +0200

    passed mod1

--- exercises/Mod01/ex00/Zombie.cpp
+++ exercises/Mod01/ex00/Zombie.cpp
@@ -2,15 +2,18 @@
 #include "Zombie.hpp"
 
 Zombie::Zombie(std::string name) : _name(name) {
-	std::cout << "[A zombie named '" << _name << "' raised out of a constructor's grave]." << std::endl;
+	std::cout << "[A zombie named '" << _name 
+    << "' raised out of a constructor's grave]." << std::endl;
 }
 
 Zombie::~Zombie(){
-	std::cout << "[BANG!] " << Zombie::_name << " was killed by a destructor.\n" << std::endl;
+	std::cout << "[BANG!] " << Zombie::_name 
+    << " was killed by a destructor.\n" << std::endl;
 }
 
 void Zombie::announce(void) {
-	std::cout << this->_name << ": Braiiiiiiinnnssss..." << std::endl;
+	std::cout << this->_name 
+    << ": Braiiiiiiinnnssss..." << std::endl;
 }
 
 std:: string Zombie::name[8] = {
