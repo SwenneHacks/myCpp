465b1d2c5646e4771fad2968f030811554e74696
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-01-13 15:27:42 +0100

    start

--- modulos/Day02/ex02/Fixed.cpp
+++ modulos/Day02/ex02/Fixed.cpp
@@ -1,55 +1,53 @@
-#include <Fixed.hpp>
+#include "Fixed.hpp"
 
 #include <string>
 #include <iostream>
 
-Fixed::Fixed()
-{ std::cout << "Default constructor called" << std::endl;}
+Fixed::Fixed(){ 
+	std::cout << "Default constructor called" << std::endl;
+}
 
-Fixed::Fixed(const Fixed &copy)
-{ std::cout << "Copy constructor called" << std::endl; *this = copy;}
+Fixed::Fixed(const Fixed &copy){ 
+	std::cout << "Copy constructor called" << std::endl; *this = copy;
+}
 
-Fixed::~Fixed()
-{ std::cout << "Destructor called" << std::endl; }
+Fixed::~Fixed(){ 
+	std::cout << "Destructor called" << std::endl; 
+}
 
-Fixed&	Fixed::operator=(const Fixed& ref)
-{ 	std::cout << "Standard Assignation operator called" << std::endl; 
+Fixed&	Fixed::operator=(const Fixed& ref){ 	
+	std::cout << "Standard Assignation operator called" << std::endl; 
 	return(*this); 
+}
+
+std::ostream	&operator<<(std::ostream &o, const Fixed &ref){	
+	o << ref.toFloat();
+	return (o);
 }
 
 /* COMPARISON OPERATORS */
 
-bool Fixed::operator<(const Fixed& A) const
-{}
+bool Fixed::operator<(const Fixed& A) const{}
 
-bool Fixed::operator>(const Fixed& A) const
-{}
+bool Fixed::operator>(const Fixed& A) const{}
 
-bool Fixed::operator==(const Fixed& A) const
-{}
+bool Fixed::operator==(const Fixed& A) const{}
 
-bool Fixed::operator!=(const Fixed& A) const
-{}
+bool Fixed::operator!=(const Fixed& A) const{}
 
-bool Fixed::operator>=(const Fixed& A) const
-{}
+bool Fixed::operator>=(const Fixed& A) const{}
 
-bool Fixed::operator<=(const Fixed& A) const
-{}
+bool Fixed::operator<=(const Fixed& A) const{}
 
 /* ARITHMETIC OPERATORS */
 
-Fixed	Fixed::operator+(const Fixed& A) const
-{}
+Fixed	Fixed::operator+(const Fixed& A) const{}
 
-Fixed	Fixed::operator-(const Fixed& A) const
-{}
+Fixed	Fixed::operator-(const Fixed& A) const{}
 
-Fixed	Fixed::operator*(const Fixed& A) const
-{}
+Fixed	Fixed::operator*(const Fixed& A) const{}
 
-Fixed	Fixed::operator/(const Fixed& A) const
-{}
+Fixed	Fixed::operator/(const Fixed& A) const{}
 
 //* INC/DEC-METRIC OPERATORS */
 
