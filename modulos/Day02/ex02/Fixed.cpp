54a16137a9e29a2e0d85b6b52e269ca12ed39622
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-02-02 15:19:39 +0100

    new comment

--- modulos/Day02/ex02/Fixed.cpp
+++ modulos/Day02/ex02/Fixed.cpp
@@ -3,29 +3,28 @@
 #include <string>
 #include <iostream>
 
-Fixed::Fixed(){ 
-	std::cout << "Default constructor called" << std::endl;
+Fixed::Fixed(){ std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed &copy){ 
-	std::cout << "Copy constructor called" << std::endl; *this = copy;
+Fixed::Fixed(const Fixed &copy){ std::cout << "Copy constructor called" << std::endl; 
+	*this = copy;
 }
 
-Fixed::~Fixed(){ 
-	std::cout << "Destructor called" << std::endl; 
+Fixed::~Fixed(){ std::cout << "Destructor called" << std::endl; 
 }
 
-Fixed&	Fixed::operator=(const Fixed& ref){ 	
-	std::cout << "Standard Assignation operator called" << std::endl; 
+Fixed&	Fixed::operator=(const Fixed& ref){ std::cout << "Assignation (=) operator called" << std::endl; 
 	return(*this); 
 }
 
-std::ostream	&operator<<(std::ostream &o, const Fixed &ref){	
+std::ostream	&operator<<(std::ostream &o, const Fixed &ref){	std::cout << "Ostream (<<) operator called" << std::endl; 
 	o << ref.toFloat();
 	return (o);
 }
 
-/* COMPARISON OPERATORS */
+/* 
+** COMPARISON
+*/
 
 bool Fixed::operator<(const Fixed& A) const { return (this->_fp < A._fp); }
 
@@ -39,7 +38,9 @@ bool Fixed::operator>=(const Fixed& A) const { return (this->_fp >= A._fp); }
 
 bool Fixed::operator<=(const Fixed& A) const { return (this->_fp <= A._fp); }
 
-/* ARITHMETIC OPERATORS */
+/*
+** ARITHMETIC
+*/
 
 Fixed	Fixed::operator+(const Fixed& A) const { return (Fixed(toInt() + A.toInt())); }
 
@@ -49,21 +50,25 @@ Fixed	Fixed::operator*(const Fixed& A) const { return (Fixed(toInt() * A.toInt()
 
 Fixed	Fixed::operator/(const Fixed& A) const { return (Fixed(toInt() / A.toInt())); }
 
-//* INC/DEC-METRIC OPERATORS */
+/* 
+** INC/DEC-METRIC 
+*/
 
 Fixed	Fixed::operator++( int value ) 
 {
-	value++;
+	++(this->value);
 	return (value);
 }
 
 Fixed	Fixed::operator--( int value ) 
 {
-	value--;
+	--(*this->value);
 	return (value);
 }
 
-/* MAX/MIN FUNCTIONS */
+/* 
+** MAX/MIN FUNCTIONS 
+*/
 
 Fixed	&Fixed::min(Fixed &first, Fixed &second)
 {
