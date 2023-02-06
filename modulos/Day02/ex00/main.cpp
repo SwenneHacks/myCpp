d53e1bd18485884bc5fb7c1904101948e616956a
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-02-06 15:18:20 +0100

    new tests

--- modulos/Day02/ex00/main.cpp
+++ modulos/Day02/ex00/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include "Fixed.hpp"
 
+void printFixeNr(Fixed f) { 
+		std::cout 
+	<<  f.getRawBits() 
+	<< "(copy out of scope)" 
+	<< std::endl;
+}
+
 void test(void) 
 {
+	std::cout
+<< RED << "____________"
+<< BLUE << " SUBJECT "
+<< RED << "____________"
+<< RESET << std::endl;
+
 Fixed a;
 Fixed b( a );
 Fixed c;
@@ -11,25 +24,42 @@ std::cout << a.getRawBits() << std::endl;
 std::cout << b.getRawBits() << std::endl;
 std::cout << c.getRawBits() << std::endl;
 
-std::cout
-<< RED << "_________"
-<< BLUE << "MY TEST"
-<< RED << "________\n"
-<< RESET << 
-std::endl;
+	std::cout
+<< RED << "____________"
+<< BLUE << " MY TEST "
+<< RED << "____________"
+<< RESET << std::endl;
 
-a.setRawBits(55 / 7);
-std::cout << a.getRawBits() <<" (a) "<< std::endl;
-b.setRawBits(000000000007);
-std::cout << b.getRawBits() <<" (b) "<< std::endl;
-c = a;
-std::cout << c.getRawBits() <<" (c) "<< std::endl;
+int number;
+std::cout << YELLOW 
+<< "Now choose a number: " ;
+std::cin >> number;
+std::cout << RESET;
+
+// CHOOSING NUMBERS
+b.setRawBits(number);
+std::cout << b.getRawBits() 
+<< " (b = choice) " << std::endl;
+
+// DIVIDING NUMBERS
+c.setRawBits(5 / 3);
+std::cout << c.getRawBits() 
+<< " (b = 5 / 3) "<< std::endl;
+
+
+// COPY OUT OF SCOPE 
+Fixed d(b);
+printFixeNr(d);
+std::cout << d.getRawBits() 
+<< " d(copyed value) " << std::endl;
+
+// (remove *this = copy)
 
 }
 
 void	check_leaks(int ac, char **av)
 {
-	std::cout << std::endl;
+	std::cout << RED << std::endl;
 	std::string	s = av[0];
 	s = "leaks " + s.erase(0,2);
 	if (ac > 1 && !strcmp(av[1],"leaks"))
