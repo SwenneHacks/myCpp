6ee21dee08cea6e9893484a10bb36a471e06f4c3
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-02-06 15:18:39 +0100

    copy constructors cool now

--- modulos/Day02/ex00/Fixed.cpp
+++ modulos/Day02/ex00/Fixed.cpp
@@ -8,8 +8,8 @@ const int Fixed::_bits = 8;
 Fixed::Fixed() : _value(0){ std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed &src){ std::cout << "Copy constructor called" << std::endl;
-    *this = src;
+Fixed::Fixed(const Fixed &copy){ std::cout << "Copy constructor called" << std::endl;
+    *this = copy;
 }
 
 Fixed& Fixed::operator=(const Fixed& ref){ std::cout << "Copy assignation operator called" << std::endl;
@@ -27,5 +27,5 @@ void Fixed::setRawBits(int const new_value){ std::cout << "setRawBits member fun
 	return ;
 }
 
-Fixed::~Fixed(){ std::cout << "Destructor called" << std::endl;
+Fixed::~Fixed(){ std::cout << "Destructor called for value: " << _value << std::endl;
 }
