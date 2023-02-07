af6f262c95febd466dd25f8d3f2d002da895514d
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-02-07 18:52:36 +0100

    super test

--- modulos/Day02/ex00/Fixed.hpp
+++ modulos/Day02/ex00/Fixed.hpp
@@ -5,8 +5,8 @@
 # define RED			"\033[31m"
 # define BLUE			"\033[34m"	
 # define GREEN			"\033[32m"	
-# define RESET			"\033[0m"
 # define YELLOW			"\x1b[33m"
+# define RESET			"\033[0m\n"
 
 class	Fixed
 {
@@ -20,7 +20,6 @@ public:
 		int			getRawBits(void) const;
 		void		setRawBits(int const raw);
 		~Fixed();
-
 };
 
 #endif
--- modulos/Day02/ex00/main.cpp
+++ modulos/Day02/ex00/main.cpp
@@ -4,78 +4,81 @@
 void printFixeNr(Fixed f) { 
 		std::cout 
 	<<  f.getRawBits() 
-	<< "(copy out of scope)" 
-	<< std::endl;
+	<< YELLOW << " (copy out of scope)" 
+	<< RESET << std::endl;
 }
 
-void test(void) 
-{
+void my_own_tests(){
 	std::cout
-<< RED << "____________"
-<< BLUE << " SUBJECT "
-<< RED << "____________"
-<< RESET << std::endl;
+	<< RED << "____________"
+	<< BLUE << " MY TEST "
+	<< RED << "____________"
+	<< RESET << 
+	std::endl;
 
-Fixed a;
-Fixed b( a );
-Fixed c;
-c = b;
-std::cout << a.getRawBits() << std::endl;
-std::cout << b.getRawBits() << std::endl;
-std::cout << c.getRawBits() << std::endl;
+	int number;
+	std::cout << YELLOW 
+	<< "Now choose a number: " ;
+	std::cin >> number;
+	std::cout << RESET;
 
-	std::cout
-<< RED << "____________"
-<< BLUE << " MY TEST "
-<< RED << "____________"
-<< RESET << std::endl;
+	Fixed a;
+	// CHOOSING NUMBERS
+	a.setRawBits(number);
+	std::cout << a.getRawBits() 
+	<< RED << " (b = choice) " 
+	<< RESET << std::endl;
 
-int number;
-std::cout << YELLOW 
-<< "Now choose a number: " ;
-std::cin >> number;
-std::cout << RESET;
+	Fixed b;
+	// DIVIDING NUMBERS
+	b.setRawBits(5 / 3);
+	std::cout << b.getRawBits() 
+	<< BLUE << " (b = 5 / 3) "
+	<< RESET << std::endl;
 
-// CHOOSING NUMBERS
-b.setRawBits(number);
-std::cout << b.getRawBits() 
-<< " (b = choice) " << std::endl;
-
-// DIVIDING NUMBERS
-c.setRawBits(5 / 3);
-std::cout << c.getRawBits() 
-<< " (b = 5 / 3) "<< std::endl;
-
-
-// COPY OUT OF SCOPE 
-Fixed d(b);
-printFixeNr(d);
-std::cout << d.getRawBits() 
-<< " d(copyed value) " << std::endl;
-
-// (remove *this = copy)
-
+	// COPY OUT OF SCOPE 
+	Fixed d(a);
+	printFixeNr(d);
+	std::cout << d.getRawBits() <<
+	GREEN << " d(copied value) " << 
+	RESET << std::endl;
+	// (remove *this = copy in Fixed.cpp file)
 }
 
-void	check_leaks(int ac, char **av)
-{
+void subject_test(){
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+}
+
+void check_leaks(char **av){
 	std::cout << RED << std::endl;
 	std::string	s = av[0];
 	s = "leaks " + s.erase(0,2);
-	if (ac > 1 && !strcmp(av[1],"leaks"))
-		system(s.c_str());
-	else {
-		s += " >> leaks.txt";
-		system(s.c_str());
-		system("grep \"total leaked bytes\" leaks.txt");
-		system("rm leaks.txt");
-	}
+	s += " >> leaks.txt";
+	system(s.c_str());
+	system("grep \"total leaked bytes\" leaks.txt");
+	system("rm leaks.txt");
 }
 
-int		main(int ac, char **av)
-{
-	test();
-	check_leaks(ac, av);
+int	 main(int ac, char **av){
+	std::cout
+	<< RED << "____________"
+	<< BLUE << " SUBJECT "
+	<< RED << "____________"
+	<< RESET << 
+	std::endl;
+
+	subject_test();
+	my_own_tests();
+
+	(void)ac;
+	check_leaks(av);
+
 	return (0);
 }
 
