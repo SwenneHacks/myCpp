3dcf9fc55e0e11b955b5de027389770acfaa4eaa
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-02-23 18:54:41 +0100

    created ex01

---
+++ modulos/Day03/ex01/ScavTrap.cpp
@@ -0,0 +1,16 @@
+#include "ScavTrap.hpp"
+
+#include <string>
+#include <iostream>
+
+ScavTrap::ScavTrap()
+{}
+
+ScavTrap::ScavTrap(const ScavTrap& ref)
+{}
+
+ScavTrap&	ScavTrap::operator=(const ScavTrap& ref)
+{}
+
+ScavTrap::~ScavTrap()
+{}
