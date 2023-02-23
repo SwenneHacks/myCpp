776c23399368ff2fbb8935df633694f2e39baeb6
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-02-23 18:54:50 +0100

    created 2

---
+++ modulos/Day03/ex02/FragTrap.cpp
@@ -0,0 +1,16 @@
+#include "FragTrap.hpp"
+
+#include <string>
+#include <iostream>
+
+FragTrap::FragTrap()
+{}
+
+FragTrap::FragTrap(const FragTrap& ref)
+{}
+
+FragTrap&	FragTrap::operator=(const FragTrap& ref)
+{}
+
+FragTrap::~FragTrap()
+{}
