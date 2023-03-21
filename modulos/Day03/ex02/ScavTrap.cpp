ba85d31793dfd56b10a7d8bf5eec757a91a4ecc0
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-03-21 17:14:52 +0100

    6

--- modulos/Day03/ex02/ScavTrap.cpp
+++ modulos/Day03/ex02/ScavTrap.cpp
@@ -3,22 +3,23 @@
 #include <string>
 #include <iostream>
 
-ScavTrap::ScavTrap() : ClapTrap("ScavTrap") 
+ScavTrap::ScavTrap() : ClapTrap() 
 { std::cout << "Default ScavTrap has been constructed." << std::endl; }
 
 ScavTrap::ScavTrap(const ScavTrap& rhs) : ClapTrap(rhs) 
 { std::cout << "Copy constructed called on ScavTrap" << std::endl; }
 
-ScavTrap::ScavTrap(std::string &new_name) : ClapTrap(new_name)
+ScavTrap::ScavTrap(std::string new_name)
 {
 	std::cout << "ScavTrap " << new_name << " has been constructed." << std::endl;
+	_name = new_name;
 	_health = 100;
 	_energy = 50;
 	_damage = 20;
 }
 
 ScavTrap::~ScavTrap() 
-{ std::cout << "default ScavTrap has been destructed" << std::endl; }
+{ std::cout << "ScavTrap destructor called" << std::endl; }
 
 ScavTrap& ScavTrap::operator=(const ScavTrap& ref)
 { 	std::cout << "ScavTrap: Copy assignment operator called" << std::endl;
@@ -30,7 +31,7 @@ ScavTrap& ScavTrap::operator=(const ScavTrap& ref)
 }
 
 void ScavTrap::attack(const std::string& target)
-{
+{ std::cout << RED << "ATTACK [ScavTrap] " << END;
 	if (_energy > 0 && _health > 0) {
 		std::cout << "ScavTrap " << _name << " attacks " << std::endl;
         std::cout << target << " damaged with " << _damage << " points" << std::endl;
@@ -39,7 +40,6 @@ void ScavTrap::attack(const std::string& target)
 }
 
 void ScavTrap::guardGate(void)
-{
-	std::cout << "ScavTrap: Guarding the gate" << std::endl;
+{ std::cout << "ScavTrap: Guarding the gate" << std::endl;
 	return ;
 }
