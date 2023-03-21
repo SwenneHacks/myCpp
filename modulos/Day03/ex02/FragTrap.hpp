3771c6656e5909d154071980da5bb092ecbf0794
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-03-21 17:14:33 +0100

    1

--- modulos/Day03/ex02/FragTrap.hpp
+++ modulos/Day03/ex02/FragTrap.hpp
@@ -1,13 +1,21 @@
 #ifndef FRAG_TRAP_HPP
 # define FRAG_TRAP_HPP
 
-class	FragTrap
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+class	FragTrap : public ClapTrap
 {
 public:
 	FragTrap();
+	FragTrap(std::string Name);
 	FragTrap(const FragTrap& ref);
-	FragTrap& operator=(const FragTrap& ref);
 	~FragTrap();
+	// OPERATOR
+	FragTrap& operator=(const FragTrap& ref);
+	// MEMBERS
+	void attack(const std::string& target);
+	void highFiveGuys(void);
 };
 
 #endif
