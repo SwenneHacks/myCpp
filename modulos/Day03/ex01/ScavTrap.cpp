af8b7928bc15f7f0f04116ae9c1f71b0fdc67c51
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-03-27 15:38:03 +0200

    organized

--- modulos/Day03/ex01/ScavTrap.cpp
+++ modulos/Day03/ex01/ScavTrap.cpp
@@ -1,27 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ScavTrap.cpp                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/03/24 14:36:33 by swofferh      #+#    #+#                 */
+/*   Updated: 2023/03/24 15:25:34 by swofferh      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ScavTrap.hpp"
 
 #include <string>
 #include <iostream>
 
-ScavTrap::ScavTrap() : ClapTrap("ScavTrap") 
-{ std::cout << "Default ScavTrap has been constructed." << std::endl; }
+ScavTrap::ScavTrap(void) : ClapTrap("ScavTrap") 
+{ 
+	std::cout << "Default ScavTrap has been constructed." << std::endl;
+	return ;
+}
 
-ScavTrap::ScavTrap(const ScavTrap& rhs) : ClapTrap(rhs) 
-{ std::cout << "Copy constructed called on ScavTrap" << std::endl; }
 
-ScavTrap::ScavTrap(std::string &new_name) : ClapTrap(new_name)
+ScavTrap::ScavTrap(std::string new_name) : ClapTrap(new_name)
 {
-	std::cout << "ScavTrap " << new_name << " has been constructed." << std::endl;
+	std::cout << "ScavTrap " <<new_name << " has been constructed." << std::endl;
 	_health = 100;
 	_energy = 50;
 	_damage = 20;
+	return ;
 }
 
-ScavTrap::~ScavTrap() 
-{ std::cout << "default ScavTrap has been destructed" << std::endl; }
+ScavTrap::ScavTrap(const ScavTrap& copy) : ClapTrap(copy) 
+{ 
+	std::cout << "Copy constructed called on ScavTrap" << std::endl; 
+	return ;
+}
 
 ScavTrap& ScavTrap::operator=(const ScavTrap& ref)
-{ 	std::cout << "ScavTrap: Copy assignment operator called" << std::endl;
+{ 	
+	std::cout << "ScavTrap: Copy assignment operator called" << std::endl;
 	_name = ref._name;
 	_health = ref._health;
 	_energy = ref._energy;
@@ -31,15 +49,26 @@ ScavTrap& ScavTrap::operator=(const ScavTrap& ref)
 
 void ScavTrap::attack(const std::string& target)
 {
-	if (_energy > 0 && _health > 0) {
-		std::cout << "ScavTrap " << _name << " attacks " << std::endl;
-        std::cout << target << " damaged with " << _damage << " points" << std::endl;
+	std::cout << BLUE << this->_name << " ATTACK " << "- ScavTrap" << END;
+	
+	if (_energy > 0 && _health > 0) 
+	{
+		std::cout << "ScavTrap attacks " << target << std::endl;
 		_energy--;
 	}
+	else
+		std::cout << "No energy left for " << this->_name << std::endl;
+	return ;
 }
 
 void ScavTrap::guardGate(void)
 {
-	std::cout << "ScavTrap: Guarding the gate" << std::endl;
+	std::cout << GREEN << "The gate is being guarded by " << this->_name << END;
+	return ;
+}
+
+ScavTrap::~ScavTrap() 
+{ 
+	std::cout << "default ScavTrap has been destructed" << std::endl; 
 	return ;
 }
