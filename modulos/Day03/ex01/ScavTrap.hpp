7d90861145f668dd14266f2b8380579f5471868e
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-03-27 15:38:07 +0200

    organized

--- modulos/Day03/ex01/ScavTrap.hpp
+++ modulos/Day03/ex01/ScavTrap.hpp
@@ -2,12 +2,19 @@
 # define SCAV_TRAP_HPP
 #include "ClapTrap.hpp"
 
+#define YELLOW "\033[33m"
+#define BLUE "\x1b[34m"
+#define GREEN "\x1b[32m"
+#define CYAN "\x1b[35m"
+#define RED "\x1b[31m"
+#define RESET "\x1b[0m"
+#define END	"\x1b[0m\n"
 
 class	ScavTrap : public ClapTrap
 {
 public:
 	ScavTrap();
-	ScavTrap(std::string &);
+	ScavTrap(std::string new_name);
 	ScavTrap(const ScavTrap& ref);
 	~ScavTrap();
 	// OPERATOR
