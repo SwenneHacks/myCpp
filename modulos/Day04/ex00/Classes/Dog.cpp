df3361ba3fbd280968d9a6dbca6115faae3a5233
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-04-03 15:58:12 +0200

    5

--- modulos/Day04/ex00/Classes/Dog.cpp
+++ modulos/Day04/ex00/Classes/Dog.cpp
@@ -6,40 +6,42 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/23 17:31:03 by swofferh      #+#    #+#                 */
-/*   Updated: 2023/03/23 18:13:21 by swofferh      ########   odam.nl         */
+/*   Updated: 2023/04/03 15:56:02 by swofferh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
 
+static std::string DOG = GREEN "[dog] " RESET;
+
 Dog::Dog(void) : Animal("Dog")
 {
-	std::cout <<GREEN<< "Dog: " <<RESET<< "Default construtor called" << std::endl;
+	std::cout << DOG << "	 Default construtor called" << std::endl;
 	return ;
 }
 
 Dog::Dog(const Dog& ref)
 {
 	*this = ref;
-	std::cout << GREEN << "Dog: " << RESET << "Copy construtor called" << std::endl;
-	return ;
-}
-
-Dog::~Dog(void)
-{
-	std::cout <<GREEN<< "Dog: " <<RESET<< "Destructor called" << std::endl;
+	std::cout << DOG << "	Copy construtor called" << std::endl;
 	return ;
 }
 
 Dog& Dog::operator=(const Dog& ref)
 {
-	std::cout <<GREEN<< "Dog: " <<RESET<< "Assignment operator" << std::endl;
+	std::cout << DOG << "	Assignment operator" << std::endl;
 	this->_type = ref._type;
 	return (*this);
 }
 
 void Dog::makeSound(void) const
 {
-	std::cout <<GREEN<< "Dog: " <<RESET<< "WOoooeeef" << std::endl;
+	std::cout << "Woeff" << std::endl;
+	return ;
+}
+
+Dog::~Dog(void)
+{
+	std::cout << DOG << "	 Destructor called" << std::endl;
 	return ;
 }
