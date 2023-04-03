2b56798dc48ce5a889875749c577e33e20f1c3f3
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-04-03 15:58:17 +0200

    5

--- modulos/Day04/ex00/Classes/WrongAnimal.cpp
+++ modulos/Day04/ex00/Classes/WrongAnimal.cpp
@@ -6,52 +6,54 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/23 17:30:10 by swofferh      #+#    #+#                 */
-/*   Updated: 2023/03/23 18:22:32 by swofferh      ########   odam.nl         */
+/*   Updated: 2023/04/03 15:35:34 by swofferh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "WrongAnimal.hpp"
-#include "../colors.hpp"
+#include "Animal.hpp"
+
+static std::string WRONGANIMAL = CYAN "[Wrong Animal] " RESET;
 
 WrongAnimal::WrongAnimal(void) : _type("WrongAnimal")
 {
-	std::cout <<RED<< "WrongAnimal:" <<RESET<< " Default construtor called" << std::endl;
+	std::cout << WRONGANIMAL << "	Default construtor called" << std::endl;
 	return ;
 }
 
 WrongAnimal::WrongAnimal(const std::string& kind) : _type(kind)
 {
-	std::cout <<RED<< "WrongAnimal:" <<RESET<< " Default construtor called" << std::endl;
+	std::cout << WRONGANIMAL << "	Inherited construtor called for " << this->_type << std::endl;
 	return ;
 }
 
 WrongAnimal::WrongAnimal(const WrongAnimal& ref)
 {
 	*this = ref;
-	std::cout <<RED<< "WrongAnimal:" <<RESET<< " Copy construtor called" << std::endl;
-	return ;
-}
-
-WrongAnimal::~WrongAnimal(void)
-{
-	std::cout <<RED<< "WrongAnimal:" <<RESET<< " Destructor called" << std::endl;
+	std::cout << WRONGANIMAL << "	Copy construtor called" << std::endl;
 	return ;
 }
 
 WrongAnimal& WrongAnimal::operator=(const WrongAnimal& ref)
 {
-	std::cout <<RED<< "WrongAnimal:" <<RESET<< " Assignment operator" << std::endl;
+	std::cout << WRONGANIMAL << "	Assignment operator" << std::endl;
 	this->_type = ref._type;
 	return (*this);
 }
 
 void WrongAnimal::makeSound(void) const
 {
-	std::cout <<RED<< "WrongAnimal:" <<RESET<< " ?????" << std::endl;
+	std::cout << "!!!!!!" << std::endl;
 	return ;
 }
 
 const std::string WrongAnimal::getType(void) const
 {
 	return (_type);
+}
+
+WrongAnimal::~WrongAnimal(void)
+{
+	std::cout << WRONGANIMAL << "	Destructor called" << std::endl;
+	return ;
 }
