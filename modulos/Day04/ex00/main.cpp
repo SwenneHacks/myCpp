10a7fcd777e1875ce3aa58b2baf4ed92c10ef36a
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-04-03 15:58:00 +0200

    2

--- modulos/Day04/ex00/main.cpp
+++ modulos/Day04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/23 17:30:53 by swofferh      #+#    #+#                 */
-/*   Updated: 2023/03/23 18:40:33 by swofferh      ########   odam.nl         */
+/*   Updated: 2023/04/03 15:56:21 by swofferh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,45 +17,77 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
 
-int main(void)
+void wrong(void)
 {
-    std::cout << "___________\n\n STARTING\n___________\n" << std::endl;
-    const Animal* meta = new Animal();
-    const Animal* dog = new Dog();
-    const Animal* cat = new Cat();
-
-    std::cout << "___________\n\n GETTERS\n___________\n" << std::endl;
-    std::cout << dog->getType() << std::endl;
-    std::cout << cat->getType() << std::endl;
-
-    std::cout << "___________\n\n SOUNDS\n___________\n" << std::endl;
-    cat->makeSound(); // will output the cat sound!
-    dog->makeSound();
-    meta->makeSound();
-
-    std::cout << "___________\n\n DELETE\n___________\n" << std::endl;
-    delete meta;
-    delete dog;
-    delete cat;
-
-    std::cout << "___________\n\nWRONG ONES\n___________\n" << std::endl;
+    std::cout << "__________________________" << std::endl;
+    std::cout << "        \n WRONG "            << std::endl;
+    std::cout << "__________________________" << std::endl;
     const WrongAnimal* a = new WrongAnimal();
     const WrongAnimal* b = new WrongCat();
     const WrongCat* c = new WrongCat();
 
-    std::cout << "___________\n\n GETTERS\n___________\n" << std::endl;
-    std::cout << a->getType() << std::endl;
-    std::cout << b->getType() << std::endl;
-    std::cout << c->getType() << std::endl;
-
-    std::cout << "___________\n\n SOUNDS\n___________\n" << std::endl;
-    a->makeSound(); //will output of Animal sound
+    std::cout << "__________________________" << std::endl;
+    std::cout << "        \n SOUNDS"            << std::endl;
+    std::cout << "__________________________" << std::endl;
+    std::cout << RED << "getType: " << RESET << a->getType() << RED << " Sound: " << RESET;
+    a->makeSound();
+    std::cout << RED << "getType: " << RESET << b->getType() << RED << "    Sound: " << RESET;
     b->makeSound();
+    std::cout << RED << "getType: " << RESET << c->getType() << RED << "    Sound: " << RESET;
     c->makeSound();
 
-    std::cout << "___________\n\n DELETE\n___________\n" << std::endl;
-    delete a;
+    std::cout << "__________________________" << std::endl;
+    std::cout << "        \n DELETE"            << std::endl;
+    std::cout << "__________________________" << std::endl;
     delete b;
     delete c;
-    return 0;
+}
+
+void normal(void)
+{
+    std::cout << "__________________________" << std::endl;
+    std::cout << "        \n ANIMAL"            << std::endl;
+    std::cout << "__________________________" << std::endl;
+    const Animal* animal = new Animal();
+    const Animal* dog = new Dog();
+    const Animal* cat = new Cat();
+
+    std::cout << "__________________________" << std::endl;
+    std::cout << "        \n SOUNDS"            << std::endl;
+    std::cout << "__________________________" << std::endl;
+    std::cout << RED << "getType: " << RESET << dog->getType() << RED << "  Sound: " << RESET;
+    cat->makeSound();
+    std::cout << RED << "getType: " << RESET << cat->getType() << RED << "  Sound: " << RESET;
+    dog->makeSound();
+    std::cout << RED << "getType: " << RESET << animal->getType() << RED << "  Sound: " << RESET;
+    animal->makeSound();
+
+    std::cout << "__________________________" << std::endl;
+    std::cout << "        \n DELETE"            << std::endl;
+    std::cout << "__________________________" << std::endl;
+    delete animal;
+    delete dog;
+    delete cat;
+}
+
+void check_leaks(char **av)
+{
+	std::cout << RED_BOLD << "\nLEAKS: " << WHITE_BOLD << std::endl;
+	std::string	s = av[0];
+	s = "leaks " + s.erase(0,2);
+	s += " >> leaks.txt";
+	system(s.c_str());
+	system("grep \"total leaked bytes\" leaks.txt");
+	system("rm leaks.txt");
+}
+
+int	 main(int ac, char **av)
+{
+	normal();
+	wrong();
+
+	(void)ac;
+	check_leaks(av);
+
+	return (0);
 }
