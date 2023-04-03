2aa9706abe602bb6d9a7fa9569d4d35b6310d188
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-04-03 15:58:04 +0200

    3

--- modulos/Day04/ex00/Classes/Animal.cpp
+++ modulos/Day04/ex00/Classes/Animal.cpp
@@ -6,47 +6,49 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/23 17:31:21 by swofferh      #+#    #+#                 */
-/*   Updated: 2023/03/23 18:19:37 by swofferh      ########   odam.nl         */
+/*   Updated: 2023/04/03 15:37:01 by swofferh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
 
-Animal::Animal(void) : _type("Animal")
+static std::string ANIMAL = YELLOW "[animal] " RESET;
+
+Animal::Animal(void) : _type("???")
 {
-	std::cout << "Default construtor called for Animal" << std::endl;
+	std::cout << ANIMAL << "Default construtor called" << std::endl;
 	return ;
 }
 
 Animal::Animal(const std::string& kind) : _type(kind)
 {
-	std::cout << "Inherited construtor called for Animal" << std::endl;
+	std::cout << ANIMAL << "Inherited construtor called for " << this->_type << std::endl;
 	return ;
 }
 
 Animal::Animal(const Animal& copy)
 {
 	*this = copy;
-	std::cout << "Copy construtor called for Animal" << std::endl;
+	std::cout << ANIMAL << "Copy construtor called" << std::endl;
 	return ;
 }
 
 Animal::~Animal(void)
 {
-	std::cout << "Destructor called for Animal" << std::endl;
+	std::cout << ANIMAL << "Destructor called" << std::endl;
 	return ;
 }
 
 Animal& Animal::operator=(const Animal& ref)
 {
-	std::cout << "Assignment operator" << std::endl;
+	std::cout << ANIMAL << "Assignment operator" << std::endl;
 	this->_type = ref._type;
 	return (*this);
 }
 
 void Animal::makeSound(void) const
 {
-	std::cout << "????? sound" << std::endl;
+	std::cout << "?????" << std::endl;
 	return ;
 }
 
