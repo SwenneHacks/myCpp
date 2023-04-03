60f2b79a0df7c78725f1815d9e1d4c0d43b9341e
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-04-03 15:58:41 +0200

    7

--- modulos/Day04/ex00/Classes/WrongCat.cpp
+++ modulos/Day04/ex00/Classes/WrongCat.cpp
@@ -6,41 +6,37 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/23 17:30:26 by swofferh      #+#    #+#                 */
-/*   Updated: 2023/03/23 18:22:36 by swofferh      ########   odam.nl         */
+/*   Updated: 2023/04/03 15:14:32 by swofferh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "WrongCat.hpp"
-#include "../colors.hpp"
+#include "Animal.hpp"
+
+static std::string WRONGCAT = MAGENTA "[Wrong Cat] " RESET;
 
 WrongCat::WrongCat(void) : WrongAnimal("WrongCat")
 {
-	std::cout <<MAGENTA<< "WrongCat:" <<RESET<< " Default construtor called" << std::endl;
+	std::cout << WRONGCAT << "	Default construtor called" << std::endl;
 	return ;
 }
 
 WrongCat::WrongCat(const WrongCat& ref)
 {
 	*this = ref;
-	std::cout <<MAGENTA<< "WrongCat:" <<RESET<< " Copy construtor called" << std::endl;
-	return ;
-}
-
-WrongCat::~WrongCat(void)
-{
-	std::cout <<MAGENTA<< "WrongCat:" <<RESET<< " Destructor called" << std::endl;
+	std::cout << WRONGCAT << "	Copy construtor called" << std::endl;
 	return ;
 }
 
 WrongCat& WrongCat::operator=(const WrongCat& ref)
 {
-	std::cout <<MAGENTA<< "WrongCat:" <<RESET<< " Assignment operator" << std::endl;
+	std::cout << WRONGCAT << "	Assignment operator" << std::endl;
 	this->_type = ref._type;
 	return (*this);
 }
 
-void WrongCat::makeSound(void) const
+WrongCat::~WrongCat(void)
 {
-	std::cout <<MAGENTA<< "WrongCat:" <<RESET<< " Meouwww" << std::endl;
+	std::cout << WRONGCAT << "	Destructor called" << std::endl;
 	return ;
 }
