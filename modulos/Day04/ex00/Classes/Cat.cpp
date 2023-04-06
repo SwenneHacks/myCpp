bfa8ad9127816fa3df42b7b7c94c6ee537ab2e1f
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-04-06 13:30:08 +0200

    6

--- modulos/Day04/ex00/Classes/Cat.cpp
+++ modulos/Day04/ex00/Classes/Cat.cpp
@@ -6,7 +6,7 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/23 17:31:12 by swofferh      #+#    #+#                 */
-/*   Updated: 2023/04/03 15:55:50 by swofferh      ########   odam.nl         */
+/*   Updated: 2023/04/06 13:29:02 by swofferh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,28 @@
 static std::string CAT = BLUE "[cat] " RESET;
 
 Cat::Cat(void) : Animal("Cat")
-{
-	std::cout << CAT << "	 Default construtor called" << std::endl;
+{ std::cout << CAT << "	 Default construtor called" << std::endl;
 	return ;
 }
 
 Cat::Cat(const Cat& ref)
-{
+{ std::cout << CAT << "	Copy construtor called" << std::endl;
 	*this = ref;
-	std::cout << CAT << "	Copy construtor called" << std::endl;
 	return ;
 }
 
 Cat& Cat::operator=(const Cat& ref)
-{
-	std::cout << CAT << "	Assignment operator" << std::endl;
+{ std::cout << CAT << "	Assignment operator" << std::endl;
 	this->_type = ref._type;
 	return (*this);
 }
 
 void Cat::makeSound(void) const
-{
-	std::cout << "Miauu" << std::endl;
+{ std::cout << RED << " Sound: " << RESET << "Miauu" << std::endl;
 	return ;
 }
 
 Cat::~Cat(void)
-{
-	std::cout << CAT << "	 Destructor called" << std::endl;
+{ std::cout << CAT << "	 Destructor called" << std::endl;
 	return ;
 }
