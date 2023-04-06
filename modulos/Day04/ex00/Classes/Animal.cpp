6402f30c9b1e450cc897cf4cac3a926c58d648b8
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-04-06 13:29:59 +0200

    4

--- modulos/Day04/ex00/Classes/Animal.cpp
+++ modulos/Day04/ex00/Classes/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/23 17:31:21 by swofferh      #+#    #+#                 */
-/*   Updated: 2023/04/03 15:37:01 by swofferh      ########   odam.nl         */
+/*   Updated: 2023/04/06 13:29:06 by swofferh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,44 +15,38 @@
 static std::string ANIMAL = YELLOW "[animal] " RESET;
 
 Animal::Animal(void) : _type("???")
-{
-	std::cout << ANIMAL << "Default construtor called" << std::endl;
+{ std::cout << ANIMAL << "Default construtor called" << std::endl;
 	return ;
 }
 
 Animal::Animal(const std::string& kind) : _type(kind)
-{
-	std::cout << ANIMAL << "Inherited construtor called for " << this->_type << std::endl;
+{ std::cout << ANIMAL << "Inherited construtor called for " << this->_type << std::endl;
 	return ;
 }
 
 Animal::Animal(const Animal& copy)
-{
+{ std::cout << ANIMAL << "Copy construtor called" << std::endl;
 	*this = copy;
-	std::cout << ANIMAL << "Copy construtor called" << std::endl;
 	return ;
 }
 
 Animal::~Animal(void)
-{
-	std::cout << ANIMAL << "Destructor called" << std::endl;
+{ std::cout << ANIMAL << "Destructor called" << std::endl;
 	return ;
 }
 
 Animal& Animal::operator=(const Animal& ref)
-{
-	std::cout << ANIMAL << "Assignment operator" << std::endl;
+{ std::cout << ANIMAL << "Assignment operator" << std::endl;
 	this->_type = ref._type;
 	return (*this);
 }
 
 void Animal::makeSound(void) const
-{
-	std::cout << "?????" << std::endl;
+{ std::cout << RED << " Sound: " << RESET << "?????" << std::endl;
 	return ;
 }
 
 const std::string Animal::getType(void) const
-{
+{ std::cout << RED << "getType: " << RESET << _type ;
 	return (_type);
 }
