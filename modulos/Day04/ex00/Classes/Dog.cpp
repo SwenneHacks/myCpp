958e5b09d5e2ae00f41ab689b5dc3ac45e7916cc
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-04-06 13:30:13 +0200

    7

--- modulos/Day04/ex00/Classes/Dog.cpp
+++ modulos/Day04/ex00/Classes/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/23 17:31:03 by swofferh      #+#    #+#                 */
-/*   Updated: 2023/04/03 15:56:02 by swofferh      ########   odam.nl         */
+/*   Updated: 2023/04/06 13:28:58 by swofferh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,28 @@
 static std::string DOG = GREEN "[dog] " RESET;
 
 Dog::Dog(void) : Animal("Dog")
-{
-	std::cout << DOG << "	 Default construtor called" << std::endl;
+{ std::cout << DOG << "	 Default construtor called" << std::endl;
 	return ;
 }
 
 Dog::Dog(const Dog& ref)
-{
+{ std::cout << DOG << "	Copy construtor called" << std::endl;
 	*this = ref;
-	std::cout << DOG << "	Copy construtor called" << std::endl;
 	return ;
 }
 
 Dog& Dog::operator=(const Dog& ref)
-{
-	std::cout << DOG << "	Assignment operator" << std::endl;
+{ std::cout << DOG << "	Assignment operator" << std::endl;
 	this->_type = ref._type;
 	return (*this);
 }
 
 void Dog::makeSound(void) const
-{
-	std::cout << "Woeff" << std::endl;
+{ std::cout << RED << " Sound: " << RESET << "Woeff" << std::endl;
 	return ;
 }
 
 Dog::~Dog(void)
-{
-	std::cout << DOG << "	 Destructor called" << std::endl;
+{ std::cout << DOG << "	 Destructor called" << std::endl;
 	return ;
 }
