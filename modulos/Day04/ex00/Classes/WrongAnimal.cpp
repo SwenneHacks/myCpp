1d5fd504d2aafd9492639fd8057ddb19b39ccc47
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-04-06 13:30:18 +0200

    8

--- modulos/Day04/ex00/Classes/WrongAnimal.cpp
+++ modulos/Day04/ex00/Classes/WrongAnimal.cpp
@@ -6,7 +6,7 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/23 17:30:10 by swofferh      #+#    #+#                 */
-/*   Updated: 2023/04/03 15:35:34 by swofferh      ########   odam.nl         */
+/*   Updated: 2023/04/06 13:29:19 by swofferh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,14 @@
 
 static std::string WRONGANIMAL = CYAN "[Wrong Animal] " RESET;
 
-WrongAnimal::WrongAnimal(void) : _type("WrongAnimal")
-{
-	std::cout << WRONGANIMAL << "	Default construtor called" << std::endl;
+WrongAnimal::WrongAnimal(void) : _type("WrongOne")
+{ std::cout << WRONGANIMAL << "	Default construtor called" << std::endl;
 	return ;
 }
 
 WrongAnimal::WrongAnimal(const std::string& kind) : _type(kind)
-{
-	std::cout << WRONGANIMAL << "	Inherited construtor called for " << this->_type << std::endl;
+{ std::cout << WRONGANIMAL << "	Inherited construtor called for " 
+	<< this->_type << std::endl;
 	return ;
 }
 
@@ -35,25 +34,22 @@ WrongAnimal::WrongAnimal(const WrongAnimal& ref)
 }
 
 WrongAnimal& WrongAnimal::operator=(const WrongAnimal& ref)
-{
-	std::cout << WRONGANIMAL << "	Assignment operator" << std::endl;
+{ std::cout << WRONGANIMAL << "	Assignment operator" << std::endl;
 	this->_type = ref._type;
 	return (*this);
 }
 
 void WrongAnimal::makeSound(void) const
-{
-	std::cout << "!!!!!!" << std::endl;
+{ std::cout << RED << " Sound: " << RESET << "!!!!" << std::endl;
 	return ;
 }
 
 const std::string WrongAnimal::getType(void) const
-{
+{ std::cout << RED << "Type: " << RESET << _type;
 	return (_type);
 }
 
 WrongAnimal::~WrongAnimal(void)
-{
-	std::cout << WRONGANIMAL << "	Destructor called" << std::endl;
+{ std::cout << WRONGANIMAL << "	Destructor called" << std::endl;
 	return ;
 }
