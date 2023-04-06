cf6a306ca4bffd91673b09300b3616e6861d2d58
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-04-06 13:29:53 +0200

    3

--- modulos/Day04/ex00/main.cpp
+++ modulos/Day04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/23 17:30:53 by swofferh      #+#    #+#                 */
-/*   Updated: 2023/04/03 15:56:21 by swofferh      ########   odam.nl         */
+/*   Updated: 2023/04/06 13:27:06 by swofferh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,27 @@ void wrong(void)
     std::cout << "__________________________" << std::endl;
     std::cout << "        \n WRONG "            << std::endl;
     std::cout << "__________________________" << std::endl;
+    
     const WrongAnimal* a = new WrongAnimal();
     const WrongAnimal* b = new WrongCat();
     const WrongCat* c = new WrongCat();
-
+    
     std::cout << "__________________________" << std::endl;
     std::cout << "        \n SOUNDS"            << std::endl;
     std::cout << "__________________________" << std::endl;
-    std::cout << RED << "getType: " << RESET << a->getType() << RED << " Sound: " << RESET;
+    
+    a->getType();
     a->makeSound();
-    std::cout << RED << "getType: " << RESET << b->getType() << RED << "    Sound: " << RESET;
+    b->getType();
     b->makeSound();
-    std::cout << RED << "getType: " << RESET << c->getType() << RED << "    Sound: " << RESET;
+    c->getType();
     c->makeSound();
-
+    
     std::cout << "__________________________" << std::endl;
     std::cout << "        \n DELETE"            << std::endl;
     std::cout << "__________________________" << std::endl;
+    
+    delete a;
     delete b;
     delete c;
 }
@@ -48,23 +52,26 @@ void normal(void)
     std::cout << "__________________________" << std::endl;
     std::cout << "        \n ANIMAL"            << std::endl;
     std::cout << "__________________________" << std::endl;
+    
     const Animal* animal = new Animal();
     const Animal* dog = new Dog();
     const Animal* cat = new Cat();
-
+    
     std::cout << "__________________________" << std::endl;
     std::cout << "        \n SOUNDS"            << std::endl;
     std::cout << "__________________________" << std::endl;
-    std::cout << RED << "getType: " << RESET << dog->getType() << RED << "  Sound: " << RESET;
-    cat->makeSound();
-    std::cout << RED << "getType: " << RESET << cat->getType() << RED << "  Sound: " << RESET;
+    
+    dog->getType();
     dog->makeSound();
-    std::cout << RED << "getType: " << RESET << animal->getType() << RED << "  Sound: " << RESET;
+    cat->getType();
+    cat->makeSound();
+    animal->getType();
     animal->makeSound();
-
+    
     std::cout << "__________________________" << std::endl;
     std::cout << "        \n DELETE"            << std::endl;
     std::cout << "__________________________" << std::endl;
+    
     delete animal;
     delete dog;
     delete cat;
@@ -88,6 +95,6 @@ int	 main(int ac, char **av)
 
 	(void)ac;
 	check_leaks(av);
-
+    
 	return (0);
 }
