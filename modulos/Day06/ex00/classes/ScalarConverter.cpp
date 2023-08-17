a4380e336071e34058a034eb85e31a1e1b2050de
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-08-17 21:22:11 +0200

    do it

--- modulos/Day06/ex00/classes/ScalarConverter.cpp
+++ modulos/Day06/ex00/classes/ScalarConverter.cpp
@@ -7,51 +7,25 @@
 #define COLOR "\e[0;35m"
 #define RESET "\e[0m"
 
-void ScalarConverter::CastChar(long double value)
-{
-	std::cout << COLOR << "char: " << RESET;
-	try {
-		if (value < 32 || value > 126)
-			throw(NonDisplayable());
-		else
-			std::cout <<"'"<< static_cast<char>(value) 
-			<<"'"<< std::endl; // adding single quotes
-	}
-	catch(const std::exception& e) { std::cerr << e.what() << std::endl; }
-}
+// ScalarConverter::ScalarConverter() {}
 
-void ScalarConverter::CastInt(long double value)
-{
-	std::cout << COLOR << "int: " << RESET;
-	try {
-		if (value > INT_MAX || value < INT_MIN)
-			throw (NonDisplayable());
-		else
-			std::cout << static_cast<int>(value) 
-			<< std::endl; //checking for int max
-	}
-	catch(const std::exception& e) { std::cerr << e.what() << std::endl; }
-}
+// void ScalarConverter::CastChar(long double value)
+// {
+
+// }
+
+// void ScalarConverter::CastInt(long double value)
+// {
+
+// }
 
 
-void ScalarConverter::CastFloat(long double value, int digits)
-{
-    std::cout << COLOR << "float: " << RESET;
-	try {
-		std::cout << std::fixed << std::setprecision(digits) 
-		<< static_cast<float>(value) 
-		<< "f" << std::endl; // ading f and precision
-    }
-    catch (const std::exception& e) { throw (Impossible()); }
+// void ScalarConverter::CastFloat(long double value, int digits)
+// {
 
-}
+// }
 
-void ScalarConverter::CastDouble(long double value, int digits)
-{
-	std::cout << COLOR << "double: " << RESET;
-	try {
-		std::cout << std::fixed << std::setprecision(digits) 
-		<< static_cast<double>(value) << std::endl;
-    }
-    catch (const std::exception& e) { throw (Impossible()); }
-}
+// void ScalarConverter::CastDouble(long double value, int digits)
+// {
+
+// }
