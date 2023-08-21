6660fab7c7b673be578c7b45c92ffc9ddef1dac7
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-08-21 17:20:40 +0200

    `

---
+++ modulos/Day06/ex02/Base.h
@@ -0,0 +1,26 @@
+
+#ifndef CPP06_BASE_H
+#define CPP06_BASE_H
+
+#include <iostream>
+#include <random>
+
+class Base
+{
+public:
+    virtual ~Base();
+};
+
+enum e_ClassNumbers
+{
+    e_ClassA,
+    e_ClassB,
+    e_ClassC
+};
+
+void identify_from_pointer(Base* p);
+void identify_from_reference(Base& p);
+Base* generate(void);
+
+
+#endif //CPP06_BASE_H
