647c8103ec15eb2e43d8f196603011caa49755a7
SwenneHacks <swennehacks@gmail.com>
2023-10-19 19:07:52 +0200

    edit

---
+++ modulos/main.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/08/04 16:38:00 by swofferh      #+#    #+#                 */
+/*   Updated: 2023/08/24 12:43:06 by swofferh      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define RED "\e[0;31m"
+#define RESET "\e[0m"
+
+#include "Data.hpp"
+
+int	main(void)
+{
+    Data original;
+    original.value = 42;
+    original.test = "original";
+    std::cout << "Original value: " << original.value << std::endl;
+    
+    uintptr_t serialized = Serializer::serialize(&original);
+    //serialized += 1;
+
+    Data* deserialized = Serializer::deserialize(serialized);
+    //original.value += 1;
+    std::cout << "Deserialized value: " << deserialized->value << std::endl;
+    
+    if (deserialized == &original)
+        std::cout << "Serialization and deserialization success." << RESET << std::endl;
+    else
+        std::cout << RED << "Serialization and deserialization failed." << RESET << std::endl;
+    return 0;
+}
+
+/**
+    SERIALIZATION
+    is the process of converting an object or data structure into 
+    a format that can be stored or transmitted. In this case, 
+    we are serializing the address of the Data object.
+
+    DESERIALIZATION
+    is the reverse process of serialization, where the serialized 
+    data is converted back into its original form. Here, 
+    we are deserializing the serialized value back into a pointer to Data.
+    
+*/
